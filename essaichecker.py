56476fc09e1386177fef6a5ec67e40f1b141c09d
Fayez-zouari <64143127+Fayez-zouari@users.noreply.github.com>
2020-04-22 14:40:55 +0200

    Add files via upload

---
+++ essaichecker.py
@@ -0,0 +1,8 @@
+lnp=[Fouelen]
+np= str(input("Entrez Votre Nom & Prénom: "))
+lcin[0000]
+cin=int(input("Entrez Votre Num de la CIN: "))
+while np in lnp and cin in lcin:
+    print("Passez sans faire le test")
+while np not in lnp and cin not in lcin:
+    print("Allez faire le test")
